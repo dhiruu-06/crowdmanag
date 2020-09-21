55dd9b455b6725150dd2ee78a748a8235b50424a
dhiru-06 <dhiru3764@gmail.com>
2020-09-21 07:32:23 +0530

    Updates

---
+++ precache-manifest.9bc94ce145bad7c301d254a175048054.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed20ae9551b686591164a45de791c34c",
+    "url": "/crowdmanag/index.html"
+  },
+  {
+    "revision": "1921521643ec2570c91a",
+    "url": "/crowdmanag/static/css/main.1c95afed.chunk.css"
+  },
+  {
+    "revision": "141082c34a1c03ee5d12",
+    "url": "/crowdmanag/static/js/2.66c406b1.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/crowdmanag/static/js/2.66c406b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1921521643ec2570c91a",
+    "url": "/crowdmanag/static/js/main.d18cc19e.chunk.js"
+  },
+  {
+    "revision": "27c5b7a313640677cc49",
+    "url": "/crowdmanag/static/js/runtime-main.5ef15527.js"
+  },
+  {
+    "revision": "d13f31a46878cf6520d12ff887f6dc9c",
+    "url": "/crowdmanag/static/media/backg.d13f31a4.jpg"
+  },
+  {
+    "revision": "82ba53795d73265a1ec92d9296008643",
+    "url": "/crowdmanag/static/media/background.82ba5379.jpg"
+  },
+  {
+    "revision": "115ac118eea99bbad449b0f9d87b6302",
+    "url": "/crowdmanag/static/media/backk.115ac118.jpg"
+  },
+  {
+    "revision": "2bc3900b953af28ffff0bda79ed1eb89",
+    "url": "/crowdmanag/static/media/empty.2bc3900b.jpg"
+  }
+]);
